bb0b83fcdb88d87b65f742232515a42b09469b3d
kkunewjj63-crypto <kkunewjj63@gmail.com>
2026-04-30 13:08:42 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,22 @@
-self.addEventListener("fetch",()=>{});
+const CACHE = "app-v3";
+
+self.addEventListener("install", e=>{
+  e.waitUntil(
+    caches.open(CACHE).then(cache=>{
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./app.js",
+        "./manifest.json"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", e=>{
+  e.respondWith(
+    caches.match(e.request).then(res=>{
+      return res || fetch(e.request);
+    })
+  );
+});
